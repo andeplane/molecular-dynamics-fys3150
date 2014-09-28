3e2265ff4344ad90a4a7914316529f81cc624708
Anders Hafreager <anders@hafreager.no>
2014-09-28 21:40:15 +0200

    Added 100 atoms uniformly, randomly placed as an example. Fixed timestep. Added unitConverter example usage also to show what the units of length, velocity, time and mass are.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,12 @@ using namespace std;
 
 int main()
 {
-    double dt = 0.02;
+    double dt = 100;
+
+    cout << "One unit of length is " << UnitConverter::lengthToSI(1.0) << " meters" << endl;
+    cout << "One unit of velocity is " << UnitConverter::velocityToSI(1.0) << " meters/second" << endl;
+    cout << "One unit of time is " << UnitConverter::timeToSI(1.0) << " seconds" << endl;
+    cout << "One unit of mass is " << UnitConverter::massToSI(1.0) << " kg" << endl;
 
     System *system = new System();
     system->createFCCLattice(5, 5.26);
@@ -21,10 +26,13 @@ int main()
     system->setIntegrator(new EulerCromer());
     system->removeMomentum();
 
-    // Add one example atom. You'll have to create many such atoms in the createFCCLattice function above.
-    Atom *atom = new Atom(39.948); // Argon mass in atomic units, see http://en.wikipedia.org/wiki/Argon
-    atom->resetVelocityMaxwellian(UnitConverter::temperatureFromSI(300));
-    system->atoms().push_back(atom); // Add it to the list of atoms
+    for(int n=0; n<100; n++) {
+        // Add one example atom. You'll have to create many such atoms in the createFCCLattice function above.
+        Atom *atom = new Atom(UnitConverter::massFromSI(6.63352088e-26)); // Argon mass, see http://en.wikipedia.org/wiki/Argon
+        atom->resetVelocityMaxwellian(UnitConverter::temperatureFromSI(300));
+        atom->position.randomUniform(UnitConverter::lengthFromAngstroms(-10), UnitConverter::lengthFromAngstroms(10));
+        system->atoms().push_back(atom); // Add it to the list of atoms
+    }
 
     StatisticsSampler *statisticsSampler = new StatisticsSampler(); //
 
