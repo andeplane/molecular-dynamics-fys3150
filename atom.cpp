3975ca545aad553493a56c0a48faf20b77584290
Anders Hafreager <anders@hafreager.no>
2014-09-28 21:38:43 +0200

    Changed so resetVelocityMaxwellian uses randomGaussian in vec3.cpp.

--- atom.cpp
+++ atom.cpp
@@ -17,7 +17,5 @@ void Atom::resetVelocityMaxwellian(double temperature)
 {
     // Resetting the velocity according to a Maxwell-Boltzmann distribution (see http://en.wikipedia.org/wiki/Maxwell%E2%80%93Boltzmann_distribution )
     double standardDeviation = sqrt(temperature/m_mass);
-    velocity.x = Random::nextGaussian(0, standardDeviation);
-    velocity.y = Random::nextGaussian(0, standardDeviation);
-    velocity.z = Random::nextGaussian(0, standardDeviation);
+    velocity.randomGaussian(0, standardDeviation);
 }
