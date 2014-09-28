9ac0faeef59c23c13d1e033b13c1ecf1941250e7
Anders Hafreager <anders@hafreager.no>
2014-09-28 20:39:29 +0200

    Wrote a comment or something in statisticssampler.cpp.

--- statisticssampler.cpp
+++ statisticssampler.cpp
@@ -2,10 +2,12 @@
 
 StatisticsSampler::StatisticsSampler()
 {
+
 }
 
 void StatisticsSampler::sample(System *system)
 {
+    // Here you should measure different kinds of statistical properties and save it to a file.
     sampleKineticEnergy(system);
     // ...
 }
