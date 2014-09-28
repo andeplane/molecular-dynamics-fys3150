5e2d8612151244ec69a4464a178c0d0b3779ffee
Anders Hafreager <anders@hafreager.no>
2014-09-28 21:38:23 +0200

    Indented and added randomUniform and randomGaussian in vec3.cpp.

--- math/vec3.cpp
+++ math/vec3.cpp
@@ -1,123 +1,136 @@
-#include <math/vec3.h>
 #include <cmath>
+#include <math/vec3.h>
+#include <math/random.h>
 namespace CompPhys {
-    vec3::vec3() :
-        x(0),
-        y(0),
-        z(0)
-    {
+vec3::vec3() :
+    x(0),
+    y(0),
+    z(0)
+{
 
+}
+
+vec3::vec3(double x_, double y_, double z_) :
+    x(x_),
+    y(y_),
+    z(z_)
+{
+
+}
+
+bool vec3::operator==(vec3 &rhs) {
+    return(x == rhs.x && y == rhs.y && z == rhs.z);
+}
+
+vec3 vec3::operator+(vec3 &rhs) {
+    return vec3( x + rhs.x,
+                 y + rhs.y,
+                 z + rhs.z);
+}
+
+vec3 vec3::operator-(vec3 &rhs) {
+    return vec3( x - rhs.x,
+                 y - rhs.y,
+                 z - rhs.z);
+}
+
+vec3 vec3::operator*(vec3 &rhs) {
+    return vec3( x * rhs.x,
+                 y * rhs.y,
+                 z * rhs.z);
+}
+
+vec3 vec3::operator/(vec3 &rhs) {
+    return vec3( x / rhs.x,
+                 y / rhs.y,
+                 z / rhs.z);
+}
+
+vec3 vec3::operator+(double scalar) {
+    return vec3(x + scalar,
+                y + scalar,
+                z + scalar);
+}
+
+vec3 vec3::operator-(double scalar) {
+    return vec3(x - scalar,
+                y - scalar,
+                z - scalar);
+}
+
+vec3 vec3::operator*(double scalar) {
+    return vec3(x * scalar,
+                y * scalar,
+                z * scalar);
+}
+
+vec3 vec3::operator/(double scalar) {
+    return vec3(x / scalar,
+                y / scalar,
+                z / scalar);
+}
+
+void vec3::addAndMultiply(vec3 &rhs, double scalar) {
+    x += rhs.x*scalar;
+    y += rhs.y*scalar;
+    z += rhs.z*scalar;
+}
+
+void vec3::add(vec3 &rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+}
+
+double vec3::dot(vec3 &rhs) {
+    return (x * rhs.x +
+            y * rhs.y +
+            z * rhs.z);
+}
+
+vec3 vec3::cross(vec3 &rhs) {
+    return vec3( y * rhs.z - z * rhs.y,
+                 z * rhs.x - x * rhs.z,
+                 x * rhs.y - y * rhs.x);
+}
+
+double vec3::length() {
+    return sqrt(lengthSquared());
+}
+
+double vec3::lengthSquared() {
+    return x*x + y*y + z*z;
+}
+
+void vec3::normalize() {
+    double myLength = length();
+    if(myLength > 0) { // Don't divide by zero...
+        x /= myLength;
+        y /= myLength;
+        z /= myLength;
     }
+}
 
-    vec3::vec3(double x_, double y_, double z_) :
-        x(x_),
-        y(y_),
-        z(z_)
-    {
+void vec3::setToZero()
+{
+    x = 0;
+    y = 0;
+    z = 0;
+}
 
-    }
+void vec3::randomUniform(double min, double max) {
+    x = min + Random::nextDouble()*2*(max - min);
+    y = min + Random::nextDouble()*2*(max - min);
+    z = min + Random::nextDouble()*2*(max - min);
+}
 
-    bool vec3::operator==(vec3 &rhs) {
-        return(x == rhs.x && y == rhs.y && z == rhs.z);
-    }
+void vec3::randomGaussian(double mean, double standardDeviation) {
+    x = Random::nextGaussian(mean, standardDeviation);
+    y = Random::nextGaussian(mean, standardDeviation);
+    z = Random::nextGaussian(mean, standardDeviation);
+}
 
-    vec3 vec3::operator+(vec3 &rhs) {
-        return vec3( x + rhs.x,
-                     y + rhs.y,
-                     z + rhs.z);
-    }
-
-    vec3 vec3::operator-(vec3 &rhs) {
-        return vec3( x - rhs.x,
-                     y - rhs.y,
-                     z - rhs.z);
-    }
-
-    vec3 vec3::operator*(vec3 &rhs) {
-        return vec3( x * rhs.x,
-                     y * rhs.y,
-                     z * rhs.z);
-    }
-
-    vec3 vec3::operator/(vec3 &rhs) {
-        return vec3( x / rhs.x,
-                     y / rhs.y,
-                     z / rhs.z);
-    }
-
-    vec3 vec3::operator+(double scalar) {
-        return vec3(x + scalar,
-                    y + scalar,
-                    z + scalar);
-    }
-
-    vec3 vec3::operator-(double scalar) {
-        return vec3(x - scalar,
-                    y - scalar,
-                    z - scalar);
-    }
-
-    vec3 vec3::operator*(double scalar) {
-        return vec3(x * scalar,
-                    y * scalar,
-                    z * scalar);
-    }
-
-    vec3 vec3::operator/(double scalar) {
-        return vec3(x / scalar,
-                    y / scalar,
-                    z / scalar);
-    }
-
-    void vec3::addAndMultiply(vec3 &rhs, double scalar) {
-        x += rhs.x*scalar;
-        y += rhs.y*scalar;
-        z += rhs.z*scalar;
-    }
-
-    void vec3::add(vec3 &rhs) {
-        x += rhs.x;
-        y += rhs.y;
-        z += rhs.z;
-    }
-
-    double vec3::dot(vec3 &rhs) {
-        return (x * rhs.x +
-                y * rhs.y +
-                z * rhs.z);
-    }
-
-    vec3 vec3::cross(vec3 &rhs) {
-        return vec3( y * rhs.z - z * rhs.y,
-                     z * rhs.x - x * rhs.z,
-                     x * rhs.y - y * rhs.x);
-    }
-
-    double vec3::length() {
-        return sqrt(lengthSquared());
-    }
-
-    double vec3::lengthSquared() {
-        return x*x + y*y + z*z;
-    }
-
-    void vec3::normalize() {
-        double myLength = length();
-        if(myLength > 0) { // Don't divide by zero...
-            x /= myLength;
-            y /= myLength;
-            z /= myLength;
-        }
-    }
-
-    void vec3::setToZero()
-    {
-        x = 0;
-        y = 0;
-        z = 0;
-    }
-
-    std::ostream& operator<<(std::ostream &stream, vec3 &vec) {
-        return stream << "[" << vec.x << ", " << vec.y << ", " << vec.z << "]";
-    }
+std::ostream& operator<<(std::ostream &stream, vec3 &vec) {
+    return stream << "[" << vec.x << ", " << vec.y << ", " << vec.z << "]";
+}
 }
--- math/vec3.h
+++ math/vec3.h
@@ -2,33 +2,38 @@
 #include <iostream>
 
 namespace CompPhys {
-    class vec3
-    {
-    private:
-        friend std::ostream& operator<<(std::ostream&stream, vec3 &vec);
-    public:
-        vec3(); // Create a zero vector
-        vec3(double x, double y, double z);
-        bool operator==(vec3 &rhs);
-        vec3 operator+(vec3 &rhs);
-        vec3 operator-(vec3 &rhs);
-        vec3 operator*(vec3 &rhs);
-        vec3 operator/(vec3 &rhs);
-        vec3 operator+(double scalar);
-        vec3 operator-(double scalar);
-        vec3 operator*(double scalar);
-        vec3 operator/(double scalar);
-        void add(vec3 &rhs);
-        void addAndMultiply(vec3 &rhs, double scalar);
-        vec3 cross(vec3 &rhs);
-        double dot(vec3 &rhs);
-        double length();
-        double lengthSquared();
-        void normalize();
-        void setToZero();
+class vec3
+{
+public:
+    double x;
+    double y;
+    double z;
 
-        double x;
-        double y;
-        double z;
-    };
+    vec3(); // Create a zero vector
+    vec3(double x, double y, double z);
+    bool operator==(vec3 &rhs);
+    vec3 operator+(vec3 &rhs);
+    vec3 operator-(vec3 &rhs);
+    vec3 operator*(vec3 &rhs);
+    vec3 operator/(vec3 &rhs);
+    vec3 operator+(double scalar);
+    vec3 operator-(double scalar);
+    vec3 operator*(double scalar);
+    vec3 operator/(double scalar);
+    void add(vec3 &rhs);
+    void addAndMultiply(vec3 &rhs, double scalar);
+    vec3 cross(vec3 &rhs);
+    double dot(vec3 &rhs);
+    double length();
+    double lengthSquared();
+    void normalize();
+    void setToZero();
+    void randomGaussian(double mean, double standardDeviation);
+    void randomUniform(double min, double max);
+
+private:
+    friend std::ostream& operator<<(std::ostream&stream, vec3 &vec);
+
+
+};
 }
