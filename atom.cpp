6cfc8755401d133a9c9bcf3964bb6ea3edff0db8
Anders Hafreager <anders@hafreager.no>
2014-09-28 20:38:11 +0200

    Added mass and resetVelocityMaxwellian on Atom.

--- atom.cpp
+++ atom.cpp
@@ -1,11 +1,23 @@
+#include <cmath>
 #include <atom.h>
+#include <math/random.h>
 
-Atom::Atom()
+Atom::Atom(double mass) :
+    m_mass(mass)
 {
-
+    
 }
 
 void Atom::resetForce()
 {
     force.setToZero();
+}
+
+void Atom::resetVelocityMaxwellian(double temperature)
+{
+    // Resetting the velocity according to a Maxwell-Boltzmann distribution (see http://en.wikipedia.org/wiki/Maxwell%E2%80%93Boltzmann_distribution )
+    double standardDeviation = sqrt(temperature/m_mass);
+    velocity.x = Random::nextGaussian(0, standardDeviation);
+    velocity.y = Random::nextGaussian(0, standardDeviation);
+    velocity.z = Random::nextGaussian(0, standardDeviation);
 }
--- atom.h
+++ atom.h
@@ -4,11 +4,17 @@ using CompPhys::vec3;
 
 class Atom
 {
+private:
+    double m_mass;
 public:
     vec3 position;
     vec3 velocity;
     vec3 force;
 
-    Atom();
+    Atom(double mass);
     void resetForce();
+    void resetVelocityMaxwellian(double temperature);
+
+    inline double mass() { return m_mass; }
+    inline void setMass(double mass) { m_mass = mass; }
 };
