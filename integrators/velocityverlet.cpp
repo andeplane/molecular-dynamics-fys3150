b6ef1f95584295ff15e70f55e29555e8ddefddf5
Anders Hafreager <anders@hafreager.no>
2014-10-07 00:03:15 +0200

    Fixed velocity verlet class making it more clear.

--- integrators/velocityverlet.cpp
+++ integrators/velocityverlet.cpp
@@ -1,7 +1,8 @@
 #include <integrators/velocityverlet.h>
+#include <system.h>
 
 VelocityVerlet::VelocityVerlet() :
-    m_firstStep(false) // This will set the variable m_firstStep to false when the object is created
+    m_firstStep(true) // This will set the variable m_firstStep to false when the object is created
 {
 
 }
@@ -13,7 +14,9 @@ VelocityVerlet::~VelocityVerlet()
 
 void VelocityVerlet::firstKick(System *system, double dt)
 {
-
+    m_firstStep = false;
+    system->calculateForces();
+    halfKick(system, dt);
 }
 
 void VelocityVerlet::halfKick(System *system, double dt)
@@ -30,14 +33,9 @@ void VelocityVerlet::integrate(System *system, double dt)
 {
     if(m_firstStep) {
         firstKick(system, dt);
-        m_firstStep = false;
-    }
-    halfKick(system, dt);
+    } else halfKick(system, dt);
     move(system, dt);
-
-    // Step 1: Apply periodic boundary conditions
-
-    // Step 2: Calculate all forces
-
+    system->applyPeriodicBoundaryConditions();
+    system->calculateForces();
     halfKick(system, dt);
 }
